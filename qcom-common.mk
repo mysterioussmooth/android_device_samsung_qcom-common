121d003fb57fbb7d656bffacd86cbcf26a865058
Steve Kondik <shade@chemlab.org>
2013-02-06 11:13:54 -0800

    qcom-common: Move debug.mdpcomp.maxlayer setting
    Change-Id: Ife113049c4508ee4340d7b241fdd8fe8420c8bab
    
    Conflicts:
    	qcom-common.mk

--- qcom-common.mk
+++ qcom-common.mk
@@ -40,10 +40,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.mdpcomp.logs=0 \
     debug.egl.recordable.rgba8888=1
 
-ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
-    PRODUCT_PROPERTY_OVERRIDES += debug.mdpcomp.maxlayer=3
-endif
-
 # Omx
 PRODUCT_PACKAGES += \
     libdivxdrmdecrypt \
